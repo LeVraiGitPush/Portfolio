823a8e9037a34bcd45793435315e8d0be167ff7d
LeVraiGitPush <barthe_x@etna-alternance.net>
2025-06-24 17:11:55 +0200

    e

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: './', // très important pour GitHub Pages
+  base: './Portfolio', // très important pour GitHub Pages
   plugins: [react()]
 })
